2cb3e2c1d59a241fb015fbc2f98f8093a37bc169
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2019-06-27 22:34:23 -0300

    Add mobile version using cordova

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production' && process.env.CORDOVA_PLATFORM !== 'android'
     ? '/spa-tesinas'
     : '/',
+
   devServer: {
     https: true,
   },
+
   pwa: {
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
@@ -13,5 +15,9 @@ module.exports = {
         swSrc: 'public/service-worker.js',
         // ...other Workbox options...
     }
+  },
+
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
   }
 }
